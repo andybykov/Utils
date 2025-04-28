8cd6473be05d10e2024ae3147e6ccae7604d2f04
andybykov <123543480+andybykov@users.noreply.github.com>
2025-04-28 08:53:30 +0300

    Update Test.cs

--- Test.cs
+++ Test.cs
@@ -12,17 +12,37 @@ namespace Utils
             Console.WriteLine("Enter second number:");
             int y = int.Parse(Console.ReadLine());
 
-            // Проверка метода Greater() 
+            // Проверка метода Greater( ) 
             int greater = Utils.Greater(x, y);
             Console.WriteLine("The greater value is " +
             greater);
-
 
 
             // Проверка метода Swap 
             Console.WriteLine("Before swap: " + x + ", " + y);
             Utils.Swap(ref x, ref y);
             Console.WriteLine("After swap: " + x + ", " + y);
+
+            {
+
+                int factorizeResult; // Factorial result 
+                bool ok; // Factorial success or failure 
+
+                Console.WriteLine("Number for factorial:");
+                int number = int.Parse(Console.ReadLine());
+
+
+                // Проверка метода Factorial()
+                ok = Utils.Factorial(number, out factorizeResult);
+                // Вывод результатов
+                if (ok)
+                    Console.WriteLine("Factorial(" + number + ") = " + factorizeResult);
+                else
+                    Console.WriteLine("Cannot compute this factorial");
+            }
+
         }
+
+            
     }
 }
